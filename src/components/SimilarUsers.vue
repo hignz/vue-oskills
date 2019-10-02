8cf69fb9c8499f02b75a60dec286c8dd551e27d2
hignz <higz-14@live.com>
2019-10-02 02:28:42 +0100

    added icon for view switch, cleanup

--- src/components/SimilarUsers.vue
+++ src/components/SimilarUsers.vue
@@ -1,36 +1,43 @@
 <template>
-  <v-card flat>
-    <!-- <v-toolbar flat color="primary" dark>
-      <v-toolbar-title>Similar Users</v-toolbar-title>
-      <v-spacer />
-      <v-toolbar-side-icon>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-toolbar-side-icon>
-    </v-toolbar> -->
-    <v-list rounded dense="">
-      <v-subheader>SIMILAR USERS</v-subheader>
-      <v-list-item-group color="primary">
-        <v-list-item
-          v-for="(item, i) in users.data"
-          :key="i"
-          :to="{ name: 'profile', params: { id: item._id, user: item } }"
-          link
-        >
-          <v-list-item-avatar>
-            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
-          </v-list-item-avatar>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.name"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-  </v-card>
+  <v-list rounded dense>
+    <v-subheader
+      >SIMILAR USERS
+      <v-spacer></v-spacer>
+      <v-tooltip top>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <span>Change view</span>
+      </v-tooltip>
+    </v-subheader>
+    <v-list-item-group color="primary">
+      <v-list-item
+        v-for="(item, i) in users.data"
+        :key="i"
+        :to="{ name: 'profile', params: { id: item._id, user: item } }"
+        link
+      >
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.name"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-item-group>
+  </v-list>
 </template>
 
 <script>
 export default {
-  props: ['users'],
+  props: {
+    users: {
+      type: Object,
+      required: true
+    }
+  },
   methods: {
     openProfile(user) {
       this.$router.push({
