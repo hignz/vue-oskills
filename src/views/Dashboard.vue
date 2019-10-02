b1b0c011c1356e764a5b1bb58dfcf2ee4f66afa6
hignz <higz-14@live.com>
2019-10-02 02:29:27 +0100

    added SkillList to Dashboard
    cleanup

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,25 @@
 <template>
   <v-container v-if="user.skills">
+    <p class="subheading grey--text">Dashboard</p>
     <v-row>
       <v-col cols="12">
         <v-card class="pa-2" outlined tile>
-          .col-12
           <v-container>
             <v-row>
-              <v-col cols="3">Welcome, {{ user.name }}</v-col>
-              <v-col cols="3">{{ sortedSkills[0].name }}</v-col>
-              <v-col cols="3">{{ sortedSkills[1].name }}</v-col>
-              <v-col cols="3">{{ sortedSkills[2].name }}</v-col>
+              <v-col cols="3">Welcome, {{ user.name.split(' ')[0] }}</v-col>
+              <v-col cols="3">
+                <v-row>
+                  {{ sortedSkills[0].name }}
+                </v-row>
+                <v-row>
+                  <v-icon
+                    class="ml-4"
+                    :color="sortedSkills[0].esteem <= 1 ? 'orange' : 'green'"
+                  >
+                    mdi-circle
+                  </v-icon>
+                </v-row>
+              </v-col>
             </v-row>
           </v-container>
         </v-card>
@@ -31,13 +41,13 @@
 
     <v-row>
       <v-col cols="12" md="4" sm="12">
-        <v-card flat>
+        <v-card>
           <SimilarUsers :users="similarUsers" />
         </v-card>
       </v-col>
       <v-col cols="12" md="4" sm="12">
         <v-card>
-          .col-6 .col-md-4
+          <SkillList :skills="user.skills"></SkillList>
         </v-card>
       </v-col>
       <v-col cols="12" md="4" sm="12">
@@ -49,10 +59,12 @@
 
 <script>
 import SimilarUsers from '../components/SimilarUsers';
+import SkillList from '../components/SkillList';
 
 export default {
   components: {
-    SimilarUsers
+    SimilarUsers,
+    SkillList
   },
   data() {
     return {
