3bf864261881020cdb302b65384a35091f4e9724
Conor Fearon <S00169727@mail.itsligo.ie>
2019-10-03 22:33:41 +0100

    Updated endpoint

--- src/state/store.js
+++ src/state/store.js
@@ -111,7 +111,7 @@ export default new Vuex.Store({
 
       return new Promise((resolve, reject) => {
         axios
-          .post('http://localhost:1111/findusersbyname', body, config)
+          .post('http://localhost:1111/find-by-name', body, config)
           .then(response => {
             resolve(response);
           })
