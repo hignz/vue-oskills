c5a1f001793ad78a52f0ace3b88452498cbd3602
hignz <higz-14@live.com>
2019-10-11 19:29:55 +0100

    made accent color picker its own component

---
+++ src/components/AccentColorPicker.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-color-picker
+    v-model="color"
+    mode="hexa"
+    :hide-mode-switch="true"
+  ></v-color-picker>
+</template>
+
+<script>
+import vuetify from '../plugins/vuetify';
+
+export default {
+  data() {
+    return {
+      type: 'hex',
+      hex: '#696969'
+    };
+  },
+  computed: {
+    color: {
+      get() {
+        return this[this.type];
+      },
+      set(v) {
+        this[this.type] = v;
+      }
+    }
+  },
+  watch: {
+    hex: val => {
+      vuetify.framework.theme.themes.dark.primary = val;
+      vuetify.framework.theme.themes.light.primary = val;
+      localStorage.setItem('accentColor', val);
+    }
+  }
+};
+</script>
+
+<style></style>
