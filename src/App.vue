ae5b77709804a1494dc6f94c7c66045ef808dfba
hignz <higz-14@live.com>
2019-10-16 00:17:36 +0100

    fix: register page redirecting to login attempt #2

--- src/App.vue
+++ src/App.vue
@@ -61,7 +61,7 @@ export default {
       'accentColor'
     );
 
-    if (!this.getUser._id) {
+    if (this.accessToken) {
       this.$store
         .dispatch('fetchUser')
         .then(() => {})
