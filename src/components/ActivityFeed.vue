fbccdd3ab7817bd6aeb1186498c4a12aaac5e495
Kalina <S00181963@mail.itsligo.ie>
2019-10-25 00:51:42 +0100

    support for other users activity

--- src/components/ActivityFeed.vue
+++ src/components/ActivityFeed.vue
@@ -1,22 +1,14 @@
 <template>
   <v-card v-if="loaded">
     <v-toolbar dense flat>
-      <v-toolbar-title class="subtitle-2 grey--text"
-        >ACTIVITY FEED</v-toolbar-title
-      >
+      <v-toolbar-title class="subtitle-2 grey--text">ACTIVITY</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon @click="getRecentActivity()">
+      <v-btn icon @click="getActivity()">
         <v-icon>mdi-refresh</v-icon>
       </v-btn>
     </v-toolbar>
 
-    <v-list
-      dense
-      two-line
-      flat
-      class="overflow-y-auto"
-      style="max-height: 232px"
-    >
+    <v-list dense two-line flat class="overflow-y-auto" :style="maxHeight">
       <v-list-item-group color="primary">
         <v-list-item v-for="(activity, i) in activities" :key="i">
           <v-list-item-avatar>
@@ -39,17 +31,48 @@
 
 <script>
 export default {
+  props: {
+    participantId: {
+      type: String,
+      required: false,
+      default: null
+    }
+  },
   data: () => ({
     activities: null,
     loaded: false
   }),
+  computed: {
+    maxHeight() {
+      return this.participantId ? 'max-height: 350px' : 'max-height: 232px';
+    }
+  },
   created() {
-    this.getRecentActivity();
+    this.getActivity();
   },
   methods: {
+    getActivity() {
+      if (this.participantId) {
+        this.getUserActivity();
+      } else {
+        this.getRecentActivity();
+      }
+    },
     getRecentActivity() {
       this.$store
-        .dispatch('fetchActivites')
+        .dispatch('fetchRecentActivity')
+        .then(res => {
+          this.loaded = true;
+          this.activities = res;
+        })
+        .catch(err => {
+          console.log(err);
+          this.loaded = false;
+        });
+    },
+    getUserActivity() {
+      this.$store
+        .dispatch('fetchUserActivity', this.participantId)
         .then(res => {
           this.loaded = true;
           this.activities = res;
