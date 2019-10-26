c78daaf15c0f7f89d11feceb8a12e25414b7a49d
hignz <higz-14@live.com>
2019-10-26 17:35:21 +0100

    global scrollbar style

--- src/App.vue
+++ src/App.vue
@@ -84,4 +84,26 @@ a {
   text-decoration: none;
   color: white;
 }
+
+/* width */
+::-webkit-scrollbar {
+  width: 8px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  border-radius: 10px;
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
 </style>
