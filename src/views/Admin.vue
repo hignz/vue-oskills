7afd6a1af093171ab94ebda8a4c103168af80a26
hignz <higz-14@live.com>
2019-10-28 23:58:05 +0000

    Admin: made placeholder message slightly more bearable

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,5 +1,11 @@
 <template>
-  <p>YOU ARE AN ADMIN</p>
+  <v-container>
+    <v-card>
+      <v-card-text>
+        Hello, admin!
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
