fac526824e389188361c701a995eab3196034912
Kalina <S00181963@mail.itsligo.ie>
2019-10-28 01:23:36 +0000

    added list of users with this skill

--- src/views/SkillProfile.vue
+++ src/views/SkillProfile.vue
@@ -65,7 +65,47 @@
       </v-row>
     </v-card>
 
-    <v-row> </v-row>
+    <v-row>
+      <v-col cols="12" sm="12" md="4">
+        <v-card>
+          <v-toolbar dense flat>
+            <v-toolbar-title class="subtitle-2 grey--text text-uppercase"
+              >USERS WITH THIS SKILL</v-toolbar-title
+            >
+          </v-toolbar>
+
+          <v-list two-line class="overflow-y-auto" style="max-height: 345px">
+            <v-list-item
+              v-for="(owner, i) in skill.owners"
+              :key="owner._id"
+              link
+              @click="openProfile(owner._id)"
+            >
+              <v-list-item-avatar>
+                <v-img
+                  :src="`https://randomuser.me/api/portraits/men/${i}.jpg`"
+                ></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title v-text="owner.name"></v-list-item-title>
+                <v-list-item-subtitle
+                  class="grey--text"
+                  v-text="owner.role"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <EsteemBadge :esteem="owner.esteem"></EsteemBadge>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="12" md="4"> <v-card :height="392"></v-card> </v-col>
+      <v-col cols="12" sm="12" md="4">
+        <v-card :height="392"> </v-card>
+      </v-col>
+    </v-row>
     <v-snackbar v-model="showSnackbar" color="primary">
       {{ snackbarText }}
       <v-btn color="white" text @click="showSnackbar = false">
@@ -76,7 +116,12 @@
 </template>
 
 <script>
+import EsteemBadge from '../components/EsteemBadge';
+
 export default {
+  components: {
+    EsteemBadge
+  },
   data() {
     return {
       skill: {},
@@ -101,6 +146,14 @@ export default {
   methods: {
     addSkill() {
       console.log('PILLOWS');
+    },
+    openProfile(ownerId) {
+      this.$store.dispatch('updateLoading', true);
+
+      this.$router.push({
+        name: 'profile',
+        params: { id: ownerId }
+      });
     }
   }
 };
