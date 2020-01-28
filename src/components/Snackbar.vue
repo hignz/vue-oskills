47f1243c8da9fd26de82565784303e9beab9d168
hignz <higz-14@live.com>
2020-01-28 23:32:32 +0000

    experimental snackbar component

---
+++ src/components/Snackbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-snackbar v-model="showSnack">
+    <v-btn color="primary" text @click="show = false">
+      Close
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      showSnack: this.show
+    };
+  }
+};
+</script>
+
+<style></style>
