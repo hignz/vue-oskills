659654c5dcb869d04092ac9aad575114a979ccf3
hignz <higz-14@live.com>
2020-01-29 03:49:29 +0000

    Login.vue: better form validation

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,17 @@
 <template>
   <v-col sm="8" md="4">
     <v-card class="elevation-12">
-      <v-toolbar flat>
-        <v-toolbar-title>
-          Sign in to OSkills
-        </v-toolbar-title>
-      </v-toolbar>
-      <v-card-text>
-        <v-form id="loginForm" ref="loginForm" @submit.prevent="login">
+      <v-form ref="loginForm" v-model="valid" @submit.prevent="login">
+        <v-toolbar flat>
+          <v-toolbar-title>
+            Sign in to OSkills
+          </v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
           <v-text-field
             v-model="email"
             label="Email"
+            :rules="emailRules"
             prepend-inner-icon="mdi-account"
             required
           >
@@ -21,18 +22,19 @@
             prepend-inner-icon="mdi-lock"
             :append-icon="hidePassword ? 'mdi-eye-off' : 'mdi-eye'"
             :type="hidePassword ? 'password' : 'text'"
+            :rules="requiredRule"
             required
             @click:append="() => (hidePassword = !hidePassword)"
           >
           </v-text-field>
-        </v-form>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn type="submit" class="primary" form="loginForm" primary
-          >Sign In</v-btn
-        >
-      </v-card-actions>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn class="primary" primary :disabled="!valid" type="submit"
+            >Sign In</v-btn
+          >
+        </v-card-actions>
+      </v-form>
     </v-card>
   </v-col>
 </template>
@@ -43,15 +45,23 @@ import { mapActions } from 'vuex';
 export default {
   data() {
     return {
+      valid: false,
       email: '',
       password: '',
-      hidePassword: true
+      hidePassword: true,
+      emailRules: [
+        v => !!v || 'Email is required',
+        v => /.+@.+/.test(v) || 'Email must be valid'
+      ],
+      requiredRule: [v => !!v || 'Field is required']
     };
   },
   methods: {
     ...mapActions(['doLogin', 'toggleSnackbar']),
     login() {
       if (this.$refs.loginForm.validate()) {
+        console.log('aushndahbd');
+
         this.doLogin({
           email: this.email,
           password: this.password
