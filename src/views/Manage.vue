46f3b693fdc4c597a4f34a357da186d82c4b43ee
hignz <higz-14@live.com>
2020-02-02 18:47:19 +0000

    added management components and view

---
+++ src/views/Manage.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="12">
+        <ManageSkills />
+      </v-col>
+      <v-col cols="12" sm="12">
+        <ManageCategories />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import ManageSkills from '../components/ManageSkills';
+import ManageCategories from '../components/ManageCategories';
+
+export default {
+  components: {
+    ManageSkills,
+    ManageCategories
+  }
+};
+</script>
+
+<style></style>
