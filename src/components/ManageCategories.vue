d95e17266eee041d0a7b703015e15632c6d368a6
Conor Fearon <S00169727@mail.itsligo.ie>
2020-02-04 21:11:51 +0000

    Added EditCategory dialog -- Chaged categories to type empty array

--- src/components/ManageCategories.vue
+++ src/components/ManageCategories.vue
@@ -3,6 +3,7 @@
     <v-card-title
       >Manage categories
       <v-spacer></v-spacer>
+      <EditCategoryDialog></EditCategoryDialog>
       <v-text-field
         v-model="search"
         append-icon="mdi-magnify"
@@ -12,6 +13,7 @@
         hide-details
       ></v-text-field>
     </v-card-title>
+
     <v-data-table
       v-if="loaded"
       :headers="headers"
@@ -21,25 +23,47 @@
       no-results-text="No categories found"
       class="elevation-1"
     >
+      <template v-slot:item.action="{ item }">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-btn icon v-on="on">
+              <v-icon small @click="showEditDialog(item)">
+                mdi-pencil
+              </v-icon>
+            </v-btn>
+          </template>
+          <span>Edit</span>
+        </v-tooltip>
+      </template>
     </v-data-table>
   </v-card>
 </template>
 
 <script>
 import { mapActions } from 'vuex';
+import EditCategoryDialog from '../components/EditCategoryDialog';
 
 export default {
+  components: {
+    EditCategoryDialog
+  },
   data() {
     return {
       search: '',
       loaded: false,
-      categories: null,
+      categories: [],
       headers: [
         {
           text: 'Name',
           align: 'left',
           sortable: true,
           value: 'name'
+        },
+        {
+          text: 'Actions',
+          value: 'action',
+          sortable: false,
+          align: 'center'
         }
       ]
     };
