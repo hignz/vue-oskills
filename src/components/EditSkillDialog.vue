a8beb270c8746ba5ba7117cbe2b53e75dd87308f
Conor Fearon <S00169727@mail.itsligo.ie>
2020-02-09 00:35:07 +0000

    Admins can now edit skills

--- src/components/EditSkillDialog.vue
+++ src/components/EditSkillDialog.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-dialog v-model="editDialog" max-width="500" @input="v => v || close()">
+  <v-dialog
+    v-model="editSkillDialog"
+    max-width="500"
+    @input="v => v || close()"
+  >
     <template v-slot:activator="{ on }">
       <v-btn icon v-on="on">
         <v-icon small>
@@ -8,44 +12,110 @@
       </v-btn>
     </template>
     <v-card>
-      <v-card-title class="mb-4">Edit Skill</v-card-title>
-      <v-card-text>
       <v-form ref="form" v-model="valid" @submit.prevent="onSubmit">
-        <v-row>
-          <v-col cols="12">
-            <v-select
-              label="Category"
-              :items="categories"
-              item-text="text"
-              item-value="value"
-              prepend-inner-icon="mdi-playlist-star"
-              required
-              @change="populateSkills"
-            ></v-select>
-          </v-col>
-          <v-col cols="12">
-            <v-text-field
-                v-model="name"
-                label="Name"
-                :rules="requiredRules"
-                clearable
-              ></v-text-field>
-          </v-col>
+        <v-card-title class="mb-4">Edit Skill</v-card-title>
+        <v-card-text>
+          <v-select
+            v-model="category"
+            return-object
+            label="Category"
+            :items="skillCategories"
+            item-text="text"
+            item-value="value"
+            prepend-inner-icon="mdi-playlist-star"
+            required
+          ></v-select>
+          <v-text-field
+            v-model="name"
+            label="Name"
+            :rules="requiredRules"
+            prepend-inner-icon="mdi-square-edit-outline"
+            clearable
+          ></v-text-field>
         </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn text @click="close()">Close</v-btn>
-            <v-btn color="primary" :disabled="!valid" type="submit">Edit Skill</v-btn>
-          </v-card-actions>
-        </v-row>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="close()">Close</v-btn>
+          <v-btn color="primary" :disabled="!valid" type="submit"
+            >Edit Skill</v-btn
+          >
+        </v-card-actions>
       </v-form>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-export default {};
+import validationRules from '../mixins/validationRules';
+import { mapActions } from 'vuex';
+export default {
+  mixins: [validationRules],
+  props: {
+    skill: {
+      type: Object,
+      default: () => null
+    }
+  },
+  data() {
+    return {
+      valid: false,
+      name: '',
+      editSkillDialog: false,
+      categories: [],
+      category: {}
+    };
+  },
+  computed: {
+    skillCategories() {
+      return this.categories.map(c => {
+        return {
+          text: c.name,
+          value: c._id
+        };
+      });
+    }
+  },
+  watch: {
+    editSkillDialog(opened) {
+      if (opened) {
+        this.name = this.skill.name;
+      }
+    }
+  },
+  created() {
+    this.fetchCategories().then(res => {
+      this.categories = res.categories;
+    });
+  },
+  methods: {
+    ...mapActions(['editSkill', 'toggleSnackbar', 'fetchCategories']),
+    onSubmit() {
+      if (this.$refs.form.validate()) {
+        this.editSkill({
+          skillId: this.skill._id,
+          name: this.name,
+          categoryId: this.category.value
+        }).then(() => {
+          this.toggleSnackbar({
+            show: true,
+            text: 'Skill has been edited',
+            color: 'success'
+          });
+          this.$emit('update', {
+            skillId: this.skill._id,
+            categoryName: this.category.text,
+            name: this.name
+          });
+          this.close();
+        });
+      }
+    },
+    close() {
+      this.$refs.form.reset();
+      this.editSkillDialog = !this.editSkillDialog;
+    }
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
