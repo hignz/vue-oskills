109af0a07b33851823a8d63df8d3ee0d5434330c
kalina cmuchalska <S00181963@mail.itsligo.ie>
2020-02-11 11:02:43 +0000

    added resend invite functionality

--- src/components/InvitedUsers.vue
+++ src/components/InvitedUsers.vue
@@ -19,12 +19,33 @@
       <v-data-table
         :headers="headers"
         :items="users"
+        :search="searchTerm"
         item-key="email"
         no-data-text="No users loaded"
         no-results-text="No users found"
         :items-per-page="10"
       >
         <template v-slot:item.action="{ item }">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn icon v-on="on" @click="showInviteDialog(item)">
+                <v-icon small>
+                  mdi-email-send-outline
+                </v-icon>
+              </v-btn>
+            </template>
+            <span>Resend invitation</span>
+          </v-tooltip>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn icon v-on="on">
+                <v-icon small>
+                  mdi-pencil
+                </v-icon>
+              </v-btn>
+            </template>
+            <span>Edit</span>
+          </v-tooltip>
           <v-tooltip bottom>
             <template v-slot:activator="{ on }">
               <v-btn icon v-on="on">
@@ -35,18 +56,43 @@
             </template>
             <span>Delete user</span>
           </v-tooltip>
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on }">
-              <v-btn icon v-on="on">
-                <v-icon small @click="openUserProfile(item._id)">
-                  mdi-account-card-details
-                </v-icon>
-              </v-btn>
-            </template>
-            <span>User profile</span>
-          </v-tooltip>
         </template>
       </v-data-table>
+
+      <v-dialog v-model="inviteDialog" width="500" @input="v => v || close()">
+        <v-card>
+          <v-card-title class="headline" primary-title>
+            Resend Invitation
+          </v-card-title>
+
+          <v-card-text>
+            Please check if the details are correct
+            <v-spacer></v-spacer>
+            <p>{{ selectedUser.email }}</p>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer />
+            <v-btn text @click="close()">
+              Close
+            </v-btn>
+            <v-btn
+              color="error"
+              @click="
+                resendInvite(
+                  selectedUser.email,
+                  selectedUser.role,
+                  selectedUser.isAdmin
+                )
+              "
+            >
+              Send
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </template>
   </v-card>
 </template>
@@ -64,8 +110,6 @@ export default {
   data() {
     return {
       searchTerm: '',
-      expanded: [],
-      singleExpand: false,
       headers: [
         {
           text: 'Email',
@@ -86,16 +130,48 @@ export default {
           align: 'center'
         }
       ],
-      selectedUser: {}
+      selectedUser: {},
+      inviteDialog: false
     };
   },
   methods: {
-    ...mapActions(['toggleSnackbar']),
+    ...mapActions(['toggleSnackbar', 'inviteUser']),
     openUserProfile(userId) {
       this.$router.push({
         name: 'profile',
         params: { id: userId }
       });
+    },
+    showInviteDialog(item) {
+      console.log(item);
+      this.selectedUser = item;
+      console.log(this.selectedUser);
+      this.inviteDialog = true;
+    },
+    resendInvite(email, role, isAdmin) {
+      this.inviteUser({
+        email: email,
+        role: role,
+        isAdmin: isAdmin
+      })
+        .then(() => {
+          this.close();
+          this.toggleSnackbar({
+            show: true,
+            text: 'Invite has been sent',
+            color: 'success'
+          });
+        })
+        .catch(err => {
+          this.toggleSnackbar({
+            show: true,
+            text: err.response.data,
+            color: 'error'
+          });
+        });
+    },
+    close() {
+      this.inviteDialog = !this.inviteDialog;
     }
   }
 };
