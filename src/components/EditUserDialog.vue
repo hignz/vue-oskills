9cc522bac126f50af01ae4d0f1064bee7ce367d1
kalina cmuchalska <S00181963@mail.itsligo.ie>
2020-03-15 22:19:08 +0000

    created new component for edit user dailog

---
+++ src/components/EditUserDialog.vue
@@ -0,0 +1,127 @@
+<template>
+  <v-dialog v-model="dialog" width="500" @input="v => v || closeEdit()">
+    <template v-slot:activator="{ on }">
+      <v-btn icon v-on="on">
+        <v-icon small>
+          mdi-pencil
+        </v-icon>
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-card-title class="headline" primary-title>
+        Edit
+      </v-card-title>
+      <v-spacer></v-spacer>
+      <v-card-text>
+        <v-divider></v-divider>
+        <v-text-field
+          v-model="name"
+          label="Name"
+          placeholder="Name"
+          class="mt-3"
+        ></v-text-field>
+        <v-text-field
+          v-model="email"
+          label="Email"
+          placeholder="Email"
+          class="mt-3"
+        ></v-text-field>
+        <v-select
+          v-model="selectedRole"
+          label="Role"
+          :items="roles"
+          item-text="title"
+          item-value="_id"
+          return-object
+        ></v-select>
+        <v-checkbox v-model="isAdmin" label="Admin"></v-checkbox>
+        <v-spacer></v-spacer>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn text @click="close()">
+          Close
+        </v-btn>
+        <v-btn color="error" @click="editUser()">
+          Update
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  props: {
+    user: {
+      type: Object,
+      default: () => null
+    }
+  },
+  data() {
+    return {
+      roles: [],
+      dialog: false,
+      name: null,
+      email: null,
+      selectedRole: null,
+      isAdmin: false
+    };
+  },
+  watch: {
+    dialog(opened) {
+      if (opened) {
+        this.name = this.user.name;
+        this.email = this.user.email;
+        this.role = this.user.role;
+        this.isAdmin = this.user.isAdmin;
+        this.fetchRoles().then(res => {
+          this.roles = res.roles;
+        });
+      }
+    }
+  },
+  methods: {
+    ...mapActions(['toggleSnackbar', 'fetchRoles', 'adminUpdateUser']),
+    editUser() {
+      this.adminUpdateUser({
+        userId: this.user._id,
+        name: this.name,
+        email: this.email,
+        roleId: this.selectedRole._id,
+        isAdmin: this.isAdmin
+      })
+        .then(() => {
+          this.close();
+          this.toggleSnackbar({
+            show: true,
+            text: 'User updated successfully',
+            color: 'success'
+          });
+          this.$emit('update', {
+            userId: this.user._id,
+            email: this.email,
+            role: this.selectedRole,
+            isAdmin: this.isAdmin
+          });
+        })
+        .catch(err => {
+          this.toggleSnackbar({
+            show: true,
+            text: err.response.data,
+            color: 'error'
+          });
+        });
+    },
+    close() {
+      this.dialog = !this.dialog;
+    }
+  }
+};
+</script>
+
+<style></style>
